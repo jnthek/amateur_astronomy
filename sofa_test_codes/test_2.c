6c8847fda2cf34ce6617f3144bf560ff9b348817
Jishnu RRI <jishnu@rri.res.in>
2018-03-09 21:21:21 +0530

    Modified the codes for better precision

--- sofa_test_codes/test_2.c
+++ sofa_test_codes/test_2.c
@@ -6,39 +6,39 @@
 
 int main(void)
 {
-	double dl, db, dr, dd; //Everything in radian
+	double gal_longitude, gal_latitude, RA, dec; //Everything in radian
 	int iy, im, id, ihr, imn;
 	double sec;
-	double utc1,utc2;
-	double longitude,latitude;	
-	double elong,phi;
-	double aob,zob,hob,dob,rob,eo;
+	double utc1, utc2;
+	double site_longitude, site_latitude, elong, phi;
+	double azi_obs, zenith_dist_obs, hob, dob, rob, eo;
 	
 	iy = 2018;
 	im = 3;
 	id = 9;
 	ihr = 15; //Time in UTC
 	imn = 51;
-	sec = 00;
+	sec = 27;
 	
-	longitude = 77.5946; // For Bangalore
-	latitude = 12.9716;	
-	elong = deg2rad(longitude);
-	phi = deg2rad(latitude);
+	iauDtf2d("UTC", iy, im, id, ihr, imn, sec, &utc1, &utc2); //UTC Date and time fields into 2-part quasi-JD form
 	
-	dl = deg2rad(-150); //Galactic Longitude
-	db = deg2rad(-25); //Galactic Latitude
+	site_longitude = 77.5946; // For Bangalore
+	site_latitude = 12.9716;	
+	elong = deg2rad(site_longitude);
+	phi = deg2rad(site_latitude);
 	
-	iauDtf2d("UTC", iy, im, id, ihr, imn, sec, &utc1, &utc2);
-	iauG2icrs (dl, db, &dr, &dd); //Galactic to ICRS  or RA,Dec
-		
-	printf("RA = %lf \nDec = %lf\n",rad2deg(dr),rad2deg(dd));
+	gal_longitude = deg2rad(-150.7588); //Galactic Longitude
+	gal_latitude = deg2rad(-25.24533); //Galactic Latitude	
 	
-	iauAtco13(dr, dd, 0.0, 0.0, 0.0, 0.0,
+	iauG2icrs (gal_longitude, gal_latitude, &RA, &dec); //Galactic to ICRS (or RA,Dec if you will)		
+	printf("RA = %lf \nDec = %lf\n",rad2deg(RA),rad2deg(dec));
+	
+	iauAtco13(RA, dec, 0.0, 0.0, 0.0, 0.0,
               		utc1, utc2, 0.277,
               		elong, phi, 0.0, 0.0, 0.0,
               		1013, 27.0, 50.0, 1e6,
-              		&aob, &zob, &hob, &dob, &rob, &eo);
-        printf("Azimuth = %lf\nElevation = %lf\n",rad2deg(aob),90.0-rad2deg(zob));
+              		&azi_obs, &zenith_dist_obs, &hob, &dob, &rob, &eo); // This works, cross checked with Stellarium computations
+              		
+        printf("Azimuth = %lf\nElevation = %lf\n",rad2deg(azi_obs),90.0-rad2deg(zenith_dist_obs));
 	
 }
