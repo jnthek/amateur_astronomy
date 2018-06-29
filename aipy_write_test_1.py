17e44b0a5d004c678d14a7c1da081c3f564bc44c
Jishnu <jishnunambissan@gmail.com>
2018-06-29 22:03:58 +0530

    Adding astropy test codes

--- aipy_write_test_1.py
+++ aipy_write_test_1.py
@@ -1,6 +1,7 @@
 import aipy as a
 import numpy as np
 import time
+import astropy
 
 my_time = time.gmtime(time.time())
 file_name = str(my_time.tm_year)+'_'+str(my_time.tm_mon).zfill(2)+'_'+str(my_time.tm_mday).zfill(2)+'-'+str(my_time.tm_hour).zfill(2)+str(my_time.tm_min).zfill(2)+str(my_time.tm_sec).zfill(2)+".dat"
---
+++ astropy_time_test_1.py
@@ -0,0 +1,2 @@
+import astropy
+astropy.time.Time('2006-01-15 21:24:37.5', scale='utc', location=('120d', '45d'))
